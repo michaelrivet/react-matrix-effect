52cb1e3c5452678932ce237b22da890db50624e9
Mike Rivet <mrivet@uship.com>
2020-05-19 12:57:13 -0500

    feat: add MatrixCode and app that generates codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useInterval } from './useInterval';
+import { MatrixCode, MatrixCodeProps } from './MatrixCode';
 import './App.css';
 
-function App() {
+export const App = () => {
+  const maxLines = 50;
+  const windowWidth = typeof window !== 'undefined' ? window.innerWidth : 500;
+  const [currentId, setCurrentId] = useState(0);
+  const [matrixLines, setMatrixLines] = useState<MatrixCodeProps[]>([]);
+  const [autoGenerate, setAutoGenerate] = useState(false);
+
+  const clearAllCodes = () => {
+    setCurrentId(0);
+    setMatrixLines([]);
+  };
+
+  const createMatrixLine = (): MatrixCodeProps => {
+    const xPosition = Math.floor(Math.random() * windowWidth);
+    const length = Math.floor(Math.random() * 30) + 5;
+    const speed = Math.floor(Math.random() * 20) + 10;
+
+    return {
+      id: currentId,
+      xPosition,
+      length,
+      speed,
+    };
+  };
+
+  const createNewCode = () => {
+    if(!autoGenerate) return;
+    const matrixLine = createMatrixLine();
+
+    setCurrentId((currentId) => currentId + 1);
+    setMatrixLines((matrixLines) => {
+      let newMatrixLines;
+      if (matrixLines.length >= maxLines) {
+        newMatrixLines = matrixLines.slice(1);
+      } else {
+        newMatrixLines = matrixLines.slice();
+      }
+      newMatrixLines.push(matrixLine);
+      return newMatrixLines;
+    });
+  };
+  
+  useInterval(() => createNewCode(), 80);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => setAutoGenerate(!autoGenerate)} type="button">
+        { !autoGenerate ? 'Auto Generate Codes' : 'Stop Generating Codes'}
+      </button>
+      <button onClick={createNewCode} type="button">
+        Create Single Code
+      </button>
+      <button onClick={clearAllCodes} type="button">
+        Clear All
+      </button>
+      {matrixLines.map((matrixLine) => (
+        <MatrixCode
+          id={matrixLine.id}
+          key={matrixLine.id}
+          length={matrixLine.length}
+          xPosition={matrixLine.xPosition}
+          speed={matrixLine.speed}
+        />
+      ))}
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/MatrixCode.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { useInterval } from './useInterval';
+
+import './MatrixApp.scss';
+
+const characterRefreshIntervalMS = 100;
+
+const characters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+];
+
+export type MatrixCodeProps = {
+    id: number;
+    xPosition: number;
+    length: number;
+    speed: number;
+};
+
+export const MatrixCode = ({ xPosition, length, speed }: MatrixCodeProps) => {
+  const windowHeight = typeof window !== 'undefined' ? window.innerHeight : 500;
+  const [code, setCode] = useState<string[]>([]);
+  const [depth, setDepth] = useState(0);
+
+  const increaseDepth = () => {
+    if (depth > windowHeight + 400) {
+      setDepth(() => 0);
+      return;
+    }
+    setDepth((depth) => {
+      return depth + speed;
+    });
+  };
+
+  useInterval(() => {
+    const newCode = [];
+    for (let i = 0; i < length; i++) {
+      const newChar = characters[Math.floor(Math.random() * characters.length)];
+      newCode.push(newChar);
+    }
+    setCode(newCode);
+  }, characterRefreshIntervalMS);
+
+  useInterval(() => increaseDepth(), 50);
+
+  return (
+    <div
+      className="code"
+      style={{
+        position: 'absolute',
+        transform: `translate(${xPosition}px, ${depth}px)`,
+        top: -(length * 22),
+      }}
+    >
+      {code.map((character, i) => (
+        <div className="code-character" key={i}>
+          {character}
+        </div>
+      ))}
+    </div>
+  );
+};
